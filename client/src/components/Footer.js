1c405e1e47cba4f177834652547ad956e9fefb6c
Chaitra Doddi <54845012+Chaitra-D99@users.noreply.github.com>
2020-07-20 20:35:30 +0530

    Add files via upload

---
+++ client/src/components/Footer.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer(){
+    return(
+        <footer id="footer" className="page-footer black wrapper">
+        <div className="container">
+          <div className="row">
+            <div className="col l6 s12">
+              <h5 className="white-text" id="brand">Chaitra Pizza</h5>
+              <p className="grey-text text-lighten-4">Delicious pizzas for everyone</p>
+            </div>
+          </div>
+        </div>
+      </footer>
+
+    )
+}
+
+export default Footer
